caae88fc89be803bff48359471eb67bb4f00e321
韩铭 <WIN10@DESKTOP-7OBIIKJ>
2018-12-21 10:20:14 +0800

    动态菜单

---
+++ HFrame.Web/Service/CommonService.cs
@@ -0,0 +1,22 @@
+﻿using HFrame.CommonDal.Model;
+using HFrame.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HFrame.Web.Service
+{
+    public class CommonService
+    {
+        public static List<MenuTree> GetMenus()
+        {
+            return Data_Menu.Current.GetList().Select(m=>new MenuTree
+            {
+                Icon=m.Icon,
+                Link=m.LinkHref,
+                Title=m.Title
+            }).ToList();
+        }
+    }
+}
