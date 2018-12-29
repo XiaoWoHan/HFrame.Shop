5fe2b14a92637da19538beee1ee7994fb3a1552b
韩铭 <WIN10@DESKTOP-7OBIIKJ>
2018-12-29 14:33:58 +0800

    测试model

--- HFrame.Common/Helper/TestHelper.cs
+++ HFrame.Common/Helper/TestHelper.cs
@@ -10,6 +10,21 @@ namespace HFrame.Common.Helper
     {
         #region 默认值
 
+        #region 默认字符串
+        private const string _DEFAULTALERT = "<script>alert(1)</script>";
+        private const string _DEFAULTSELECT = "(SELECT * FROM DATA_SYSUSER)";
+        private const string _DEFAULTVERSION = "SELECT @@VERSION";
+        private const string _DEFAULTDBNAME = "SELECT DB_NAME()";
+        private const string _DEFAULTABLENAME = "SELECT TOP 1 NAME FROM SYSOBJECTS WHERE XTYPE='U'";
         #endregion
+
+        #region 默认时间
+        //private const DateTime _DEFAULTTIME = DateTime(0000:00:00 00:00:00:0000);
+        #endregion
+        #endregion
+        //public static T SetTestValue<T>()
+        //{
+            
+        //}
     }
 }
